b8234b47008cd54199c1910520914d205504360a
elizabel <elizabelperlazamontano@gmail.com>
2022-09-26 09:25:55 -0500

    nuevo archivo

---
+++ suma.cpp
@@ -0,0 +1,18 @@
+//Elizabeth perlaza
+//Creado por stalin
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+ float epmx,epmy,epmz;
+ cout<<"Ingrese 2 numero x y :";
+ cin>>epmx>>epmy;
+epmz=epmx+epmy;
+ cout<<"El resutado es : ";
+ cout<<epmz<<endl;
+
+ return 0;
+
+}
